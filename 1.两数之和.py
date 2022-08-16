9f6e205b201a21db6fa9eacc2314aef88f6bd17d
armin <arminzheng@qq.com>
2022-08-16 23:13:19 +0800

    两数之和改进

--- 1.两数之和.py
+++ 1.两数之和.py
@@ -1,3 +1,6 @@
+from timeit import Timer
+
+
 class Solution(object):
     # def twoSum(self, number, target):
     #     Map<Integer, Integer> hash = new HashMap<>();
@@ -10,18 +13,31 @@ class Solution(object):
     #         hash.put(numbers[i], i);
     #     }
     #     return new int[] {};
-    def twoSum(self, nums:list, target:int = 6)->list:
+    def secondTwoSum(self, nums, target):
+        source = {}
+        for i, num in enumerate(nums):
+            diff = target - num
+            if diff in source:
+                return [source[diff], i]
+            source[num] = i
+    def threeTwoSum(self, nums, target):
+        for i in range(len(nums)):
+            a = target - nums[i]
+            if a in nums and nums.index(a) != i:
+                return  i, nums.index(a)
+
+    def twoSum(self, nums: list, target: int = 6) -> list:
         '''this is stupid algorithm'''
         # two point
-        nums_index = [(v, index) for index, v in enumerate(nums)]
-        print(nums_index)
-        nums_index.sort()
+        source = [(value, index) for index, value in enumerate(nums)]
+        source.sort(key=lambda e: e[0])
+        # print(nums_index)
         begin, end = 0, len(nums) - 1
         while begin < end:
-            curr = nums_index[begin][0] + nums_index[end][0]
-            if curr == target:
-                return [nums_index[begin][1], nums_index[end][1]]
-            elif curr < target:
+            now = source[begin][0] + source[end][0]
+            if now == target:
+                return [source[begin][1], source[end][1]]
+            elif now < target:
                 begin += 1
             else:
                 end -= 1
@@ -35,5 +51,19 @@ if __name__ == '__main__':
     print(__name__)
     print(s.twoSum.__doc__)
     print(s.twoSum.__annotations__)
+
+    print("-------------")
+    print(Solution().secondTwoSum([3, 2, 4], 6))
+
+    t1 = Timer("Solution().twoSum([3, 2, 4], 6)",
+               "from __main__ import Solution")
+    print("one ", t1.timeit(number=10000), "milliseconds")
+
+    t2 = Timer(
+        "Solution().secondTwoSum([3, 2, 4], 6)", "from __main__ import Solution")
+    print("two ", t2.timeit(number=10000), "milliseconds")
+    t3 = Timer(
+        "Solution().threeTwoSum([3, 2, 4], 6)", "from __main__ import Solution")
+    print("thr ", t3.timeit(number=10000), "milliseconds")
 else:
-    print("hah")
+    print("in model")
