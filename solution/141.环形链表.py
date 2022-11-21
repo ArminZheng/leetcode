76291b38a20bd9a2f81a17129dd2da6b433da42c
armin <arminzheng@qq.com>
2022-11-22 00:13:50 +0800

    sort: change directory

---
+++ solution/141.环形链表.py
@@ -0,0 +1,40 @@
+#
+# @lc app=leetcode.cn id=141 lang=python3
+#
+# [141] 环形链表
+#
+
+# @lc code=start
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+from typing import Optional
+
+from infrastructure.node import ListNode
+
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        rabbit = tortoise = head
+        while rabbit and tortoise:
+            rabbit = rabbit.next.next
+            tortoise = tortoise.next
+            if rabbit is tortoise:
+                return True
+        return False
+# @lc code=end
+
+
+if __name__ == '__main__':
+    a = ListNode(1)
+    b = ListNode(2)
+    c = ListNode(3)
+    d = ListNode(4)
+    a.next = b
+    b.next = c
+    c.next = d
+    d.next = a
+    print(Solution().hasCycle(a))
